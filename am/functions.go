5ecf11a87f2b455f5e317b81c0c99a394b2c119a
abmutungi <abmutungi@gmail.com>
2021-10-29 18:17:27 +0100

    ascii

--- am/functions.go
+++ am/functions.go
@@ -1,22 +1,79 @@
 package am
 
 import (
-	"fmt"
+	"bufio"
 	"os"
 )
 
-func ReadFile() string {
-	fileOne, err := os.ReadFile("ascii.txt")
+// func ReadFile() string {
+// 	fileOne, err := os.ReadFile("ascii.txt")
+// 	if err != nil {
+// 		fmt.Println("Invalid input")
+// 	}
+// 	return string(fileOne)
+// }
+
+// func MakeMap() {
+// 	m := make(map[string]int)
+
+// 	start := 32
+// 	line := 1
+
+// }
+func ReadLines(path string) ([]string, error) {
+	file, err := os.Open(path)
 	if err != nil {
-		fmt.Println("Invalid input")
+		return nil, err
 	}
-	return string(fileOne)
+	defer file.Close()
+
+	var lines []string
+	scanner := bufio.NewScanner(file)
+	for scanner.Scan() {
+		lines = append(lines, scanner.Text())
+	}
+	return lines, scanner.Err()
 }
 
-func MakeMap() {
-	m := make(map[string]int)
+/*
+package main
 
-	start := 32
-	line := 1
+import (
+	"fmt"
+)
+
+func main() {
+	line1:= `"    /\     "`
+	line2:= `"   /  \    "`
+	line3:= `"  / /\ \   "`
+	line4:= `" / ____ \  "`
+
+
+	storeLine := []string{, line1,line2,line3,line4}
+
+	//fmt.Println(storeLine)
+
+	for i:= 0; i<4;i++{
+	if i==0{
+
+	}
+	fmt.Println(storeLine[i])
+	}
+
+
+
+	//map
+
+	ArnoldsCharacterMap:= make(map[int][]string)
+
+	ArnoldsCharacterMap[65] = storeLine
+
+
+	fmt.Println(ArnoldsCharacterMap)
+
+
+	storeLine2 := []string{"A", line1,line2,line3,line4}
+
 
 }
+*/
--- main.go
+++ main.go
@@ -2,16 +2,79 @@ package main
 
 import (
 	"fmt"
-	"strings"
-
+	"log"
 	"git.learn.01founders.co/abmutungi/ascii-art-test.git/am"
 )
 
 func main() {
-	slice := strings.Split(am.ReadFile(), " ")
-
-	char := 
-
-	
-	fmt.Println(char)
+	lines, err := am.ReadLines("ascii.txt")
+	if err != nil {
+		log.Fatalf("ReadLines: %s", err)
+	}
+	fmt.Println(lines)
 }
+/*
+package main
+
+import (
+	"fmt"
+)
+
+func main() {
+	myMap := make(map[int][]string)
+
+	temp:= []string{"ARNOLD"} //type of this is slice of string
+	another := []string{"MJR"}
+
+	myMap[10] = temp
+
+	fmt.Println(myMap[10])
+
+
+	//////////////////
+
+	myMap[20] = another
+
+	fmt.Println(myMap[20])
+
+	fmt.Println(myMap)
+
+	myMap[20] = []string{"Nik"}
+
+	fmt.Println(myMap)
+
+
+	//how do u add to an existing slice
+
+
+	temp = append(temp, "secondstring")
+
+	fmt.Println(temp)
+
+	//how do u add to a map??
+
+	myMap[10] = append (myMap[10], "secondstring")
+	fmt.Println(myMap[10])
+
+
+
+	////////////
+
+	start:= 35
+	for i:=0; i<50;i++{
+		myMap[start] = append (myMap[start], "line")
+		if i== 10 || i==25{
+			start ++
+		}
+
+
+	}
+
+
+	for _, line := range myMap[10]{
+		fmt.Println(line)
+	}
+
+	fmt.Println(myMap)
+}
+*/
